3a758904c05816e3ef0d23d9111ef351fd34e983
gbonk <gbonk@users.noreply.github.com>
2019-03-07 15:03:37 -0500

    Added comments for which each part is for

--- academy-award-nominees.sql
+++ academy-award-nominees.sql
@@ -1,13 +1,12 @@
-
 ### Returns the Name of the film, They earliest year the film was first published, the film's IMDB Id, and the Wikidata film object
 
-
-SELECT  ?filmLabel (MIN(YEAR(?publication_date)) AS ?year) ?imdb_id ?film   WHERE {
-  ?film wdt:P31 wd:Q11424.
-  ?film (wdt:P1411/wdt:P31?) wd:Q19020.
-  ?film wdt:P345 ?imdb_id.
-  ?film wdt:P577 ?publication_date.
+SELECT  ?filmLabel (MIN(YEAR(?publication_date)) AS ?year) ?imdb_id ?film   
+WHERE {
+  ?film wdt:P31 wd:Q11424.  # Get Films
+  ?film (wdt:P1411/wdt:P31?) wd:Q19020. #Nominated for / An instance of , and academy award
+  ?film wdt:P345 ?imdb_id.  # for each film return the IMDB ID
+  ?film wdt:P577 ?publication_date. # For each film, return their, possibly multiple, publication dates
   SERVICE wikibase:label { bd:serviceParam wikibase:language "[AUTO_LANGUAGE]". }
 }
-Group by ?filmLabel ?year ?imdb_id ?film
+Group by ?filmLabel ?year ?imdb_id ?film # Group to allow the Min(year) to work
 ORDER BY ?year ?filmLabel
