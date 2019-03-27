3c971f718be3199e797607fca355b106e0b1a5e5
gbonk <gbonk@users.noreply.github.com>
2019-03-27 14:05:58 -0400

    Switched to using the Academy Award DB Id
    I realized, when looking at my results for 2018, that I was missing a lot of films.  Turns out that however the records in Wikidata are created they are by no certain process, complete.  The more recent films, like Bohemian Rhapsody, only had their wins attached to the records.
    
    Assuming that IF the movie has a Academy DB Id, it is a movie that has at the very least, been nominated

--- academy-award-nominees.sql
+++ academy-award-nominees.sql
@@ -3,9 +3,11 @@
 SELECT  ?filmLabel (MIN(YEAR(?publication_date)) AS ?year) ?imdb_id ?film   
 WHERE {
   ?film wdt:P31 wd:Q11424.  # Get Films
-  ?film (wdt:P1411/wdt:P31?) wd:Q19020. #Nominated for / An instance of , and academy award
-  ?film wdt:P345 ?imdb_id.  # for each film return the IMDB ID
-  ?film wdt:P577 ?publication_date. # For each film, return their, possibly multiple, publication dates
+  ?film wdt:P6145 ?oscar_id. # Academy Awards Film Database Id
+  
+  OPTIONAL { ?film wdt:P577 ?publication_date. } # For each film, return their, possibly multiple, publication dates
+  OPTIONAL { ?film wdt:P345 ?imdb_id. } # for each film return the IMDB ID
+  
   SERVICE wikibase:label { bd:serviceParam wikibase:language "[AUTO_LANGUAGE]". }
 }
 Group by ?filmLabel ?year ?imdb_id ?film # Group to allow the Min(year) to work
